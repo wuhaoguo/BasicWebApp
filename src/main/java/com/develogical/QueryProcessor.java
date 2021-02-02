cc8bc139c4e3f588823dce9fb779bdd8a368ad42
wd <dw>
2021-02-02 22:56:27 +0800

    d

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,7 +8,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        else if (query.toLowerCase().contains("your name"))
+        else if (query.toLowerCase().contains("name"))
         {
             return "nihaoa";
         }
